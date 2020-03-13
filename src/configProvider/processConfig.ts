f65d4c1757c46c94064db8ccb763905e90876217
liik <385211478@qq.com>
2020-03-13 19:12:30 +0800

    修改配置文件

--- src/configProvider/processConfig.ts
+++ src/configProvider/processConfig.ts
@@ -1,41 +1,42 @@
-import { Uri, workspace } from "vscode";
+import { Uri, workspace } from "vscode"
 
 interface PathObject {
-  path: string;
-  out: string;
+  path: string
+  out: string
 }
 
 export interface SourcePath extends PathObject {
-  from: string;
+  from: string
 }
 
 export interface TemplatePath extends PathObject {
-  out: string;
-  templatePath: string;
-  excludeApiPath: string[];
+  out: string
+  templatePath: string
+  excludeApiPath: string[]
 }
 
 export type Config = {
-  source: string | string[] | SourcePath[];
-  out: string;
-  templates: TemplatePath[] | string;
-  control?: boolean;
-  generateDefination?: boolean;
-  excludeApiPath?: string[];
-};
+  source: string | string[] | SourcePath[]
+  out: string
+  templates: TemplatePath[] | string
+  groupBy?: "operationId"
+  control?: boolean
+  generateDefination?: boolean
+  excludeApiPath?: string[]
+}
 
 export default class ProcessConfig {
-  private configPath: Uri; // 配置文件路径
-  private control: boolean; // 接管模式
-  private useTypes: boolean; // 生成ts
-  private source = []; // 源数据路径
-  private out: Uri; // 输出文件夹
-  private templates = []; // 输出模板
+  private configPath: Uri // 配置文件路径
+  private control: boolean // 接管模式
+  private useTypes: boolean // 生成ts
+  private source = [] // 源数据路径
+  private out: Uri // 输出文件夹
+  private templates = [] // 输出模板
   constructor(configPath: string) {
-    this.configPath = Uri.parse(configPath);
-    this.control = false;
-    this.useTypes = false;
-    this.out = Uri.parse("");
+    this.configPath = Uri.parse(configPath)
+    this.control = false
+    this.useTypes = false
+    this.out = Uri.parse("")
   }
   // 将默认文件存入工作区
   static saveDefault() {}
--- src/configProvider/s2a.config.ts
+++ src/configProvider/s2a.config.ts
@@ -1,45 +1,43 @@
 export default {
   auto: false, // 接管全部实现，用户不可修改输出目录代码，总是会被覆盖
-  generateDefination: false, // 是否生成ts类型定义
-  source: ["http://www.example.com/swagger.json"], // 数据源路径 - path / url / {name, from: path} ,
-  out: "/exportApi",
-  // 不导出api的规则, glob
-  // excludeApiPath: ["**/path"],
-  // 转换模板，path / url / {name, from: path}
-  templates: "/.s2a/templates/template.js"
-  // templates: [
-  //   {
-  //     out: "/mock",
-  //     templatePath: "",
-  //     excludeApiPath: []
-  //   },
-  //   {
-  //     out: "/service",
-  //     templatePath: "",
-  //     excludeApiPath: []
-  //   }
-  // ]
-};
 
-// module.exports = {
-//   auto: false, // 接管全部实现，用户不可修改输出目录代码，总是会被覆盖
-//   generateDefination: false, // 是否生成ts类型定义
-//   source: ["http://www.example.com/swagger.json"], // 数据源路径 - path / url / {name, from: path} ,
-//   out: "/exportApi",
-//   // 不导出api的规则, glob
-//   // excludeApiPath: ["**/path"],
-//   // 转换模板，path / url / {name, from: path}
-//   templates: "/exportApi/templates/template.js"
-//   // templates: [
-//   //   {
-//   //     out: "/mock",
-//   //     templatePath: "",
-//   //     excludeApiPath: []
-//   //   },
-//   //   {
-//   //     out: "/service",
-//   //     templatePath: "",
-//   //     excludeApiPath: []
-//   //   }
-//   // ]
-// };
+  generateDefination: false, // 是否生成ts类型定义
+
+  source: ["http://www.example.com/swagger.json"], // 数据源路径 - path / url / {name, from: path} ,
+
+  out: "/exportApi",
+
+  groupBy: "operationId",
+
+  templates: {
+    api: "/.s2a/templates/codeTemp.js",
+    mock: "/.s2a/templates/mockTemp.js"
+  },
+
+  /**
+   * path
+   * --------
+   * templatePlugin: '../../plugin.js'
+   *
+   * function
+   * --------
+   * templatePlugins: [function aaaa() {}, function bbb () {}],
+   */
+  templatePlugins: "../../plugin.js",
+
+  /**
+   * base on url
+   * ---------------
+   * excludeApi: ['test.api.com', 'mock.api.com']
+   *
+   * base on function
+   * ---------------
+   * excludeApi: {
+   *  filter: ["methods", "url"],
+   *  expression (metaBody) {
+   *    return false
+   *  }
+   * }
+   */
+  excludeApi: ["test.api.com", "mock.api.com"]
+}
